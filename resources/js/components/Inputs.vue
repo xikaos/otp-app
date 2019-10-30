d05fa58e5ee27a724bf096320cba7a1e9c0c50d4
Francisco Zanatta <desenvolvimento@touron.com.br>
2019-10-29 22:59:15 -0300

    Geração token HOTP + TOTP funcionando.

--- resources/js/components/Inputs.vue
+++ resources/js/components/Inputs.vue
@@ -14,6 +14,9 @@
                     <div class="column">
                         <input type="text" class="input is-large" placeholder="passphrase" v-model="passphrase">
                     </div>
+                    <div v-if="implementation == 'HOTP'" class="column">
+                        <button class="button is-info is-large" @click="increaseCounter()">Event</button>
+                    </div>
                     <div class="column">
                         <button class="button is-primary is-large" @click="calculateOTP(passphrase, seed)">Submit</button>
                     </div>
@@ -29,15 +32,31 @@
                 seed: 127,
                 passphrase: "passphrase",
                 implementation: "HOTP",
-                otp: "Not calculated"
+                otp: "Not calculated",
+                counter: 0,
+                generator: require('otpauth')
             }
         },
         methods: {
             HOTP(passphrase, seed){
-                // Calculate HOTP
+                let hotp = new this.generator.HOTP({
+                    algorithm: 'SHA1',
+                    digits: 6,
+                    counter: this.counter,
+                    secret: passphrase
+                })
+                return hotp.generate()
             },
             TOTP(passphrase){
-                // Calculate TOTP
+                let totp = new this.generator.TOTP({
+                    algorithm: 'SHA1',
+                    digits: 6,
+                    period: 30,
+                    secret: passphrase
+                });
+
+                // Generate TOTP token.
+                return totp.generate();
             },
             calculateOTP(passphrase, seed){
                 if(this.implementation == "HOTP"){
@@ -45,6 +64,14 @@
                 } else {
                     this.otp = this.TOTP(passphrase);
                 }
+                this.updateOTP()
+            },
+            increaseCounter(){
+                this.counter += 1;
+                document.querySelector('#app > section:nth-child(3) > div > div > div:nth-child(1) > div > h5').innerHTML = this.counter;
+            },
+            updateOTP(){
+                document.querySelector('#otp').innerHTML = this.otp;
             }
         }
     }
